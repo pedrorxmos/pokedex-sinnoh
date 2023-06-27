8fca918b6c466d10af0ef9627f07cba8da11e04d
Pedro Ramos <pedrorxmosdev@gmail.com>
2023-06-27 22:32:10 +0200

    💄 feat(topbar): add topbar html structure

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,5 +1,33 @@
+import { Link } from 'react-router-dom';
+
 import './Topbar.scss';
 
 export const Topbar = () => {
-	return <div>Topbar</div>;
+	return (
+		<>
+			<header className="topbar">
+				<Link className="h3 logo" to="/">
+					Pokedex
+				</Link>
+
+				<nav className="nav">
+					<input type="checkbox" name="theme-toggle" id="theme-toggle" className="theme-toggle" />
+					<ul className="nav-list">
+						<li className="nav__item">
+							<Link to="/" className="nav__item">
+								Pokedex
+							</Link>
+						</li>
+						<li className="nav__item">
+							<Link to="/favorites" className="nav__item">
+								Favorites
+							</Link>
+						</li>
+					</ul>
+
+					<button className="btn menu-toggle">X</button>
+				</nav>
+			</header>
+		</>
+	);
 };
