acbb6e461d0122570596284e500c417752166f74
Pedro Ramos <pedrorxmosdev@gmail.com>
2023-06-28 21:05:29 +0200

    💄 feat(topbar): add menu-icon animation

--- src/components/MenuIcon/MenuIcon.tsx
+++ src/components/MenuIcon/MenuIcon.tsx
@@ -1,4 +1,8 @@
-export const MenuIcon = () => {
+interface MenuIconProps {
+	className?: string;
+}
+
+export const MenuIcon = ({ className }: MenuIconProps) => {
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
@@ -10,9 +14,44 @@ export const MenuIcon = () => {
 			strokeWidth="1.25"
 			strokeLinecap="round"
 			strokeLinejoin="round"
+			className={`menu-icon${className ? ' ' + className : ''}`}
 		>
-			<line x1="18" y1="15" x2="6" y2="15"></line>
-			<line x1="0" y1="7" x2="24" y2="7"></line>
+			<polyline points="0 7, 24 7" className="menu-icon__top-line">
+				<animate
+					id="menu-icon_top-line--open"
+					attributeName="points"
+					dur="0.3s"
+					fill="freeze"
+					begin="indefinite"
+					values="0 7, 24 7; 6 6, 18 18"
+				></animate>
+				<animate
+					id="menu-icon_top-line--close"
+					attributeName="points"
+					dur="0.3s"
+					fill="freeze"
+					begin="indefinite"
+					values="6 6, 18 18; 0 7, 24 7"
+				></animate>
+			</polyline>
+			<polyline points="18 15, 6 15" className="menu-icon__bottom-line">
+				<animate
+					id="menu-icon_bottom-line--open"
+					attributeName="points"
+					dur="0.3s"
+					fill="freeze"
+					begin="indefinite"
+					values="18 15, 6 15; 6 18, 18 6"
+				></animate>
+				<animate
+					id="menu-icon_bottom-line--close"
+					attributeName="points"
+					dur="0.3s"
+					fill="freeze"
+					begin="indefinite"
+					values="6 18, 18 6; 18 15, 6 15"
+				></animate>
+			</polyline>
 		</svg>
 	);
 };
