a19744fb6df512c494e0adad560ce30868361c20
Pedro Ramos <pedrorxmosdev@gmail.com>
2023-06-29 00:45:45 +0200

    💫 💄 feat(theme): add theme button animation

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -19,6 +19,13 @@ export const Topbar = () => {
 
 	const onThemeSwitch = () => {
 		theme === 'dark' ? setTheme('light') : setTheme('dark');
+
+		document.querySelector('.theme-dark')?.classList.add('animation');
+		document.querySelector('.theme-light')?.classList.add('animation');
+		setTimeout(() => {
+			document.querySelector('.theme-dark')?.classList.remove('animation');
+			document.querySelector('.theme-light')?.classList.remove('animation');
+		}, 300);
 	};
 
 	useEffect(() => {
@@ -33,7 +40,7 @@ export const Topbar = () => {
 				</Link>
 
 				<nav className="nav">
-					<button className="btn nav-action theme-toggle" title="switch theme" onClick={onThemeSwitch}>
+					<button className={`btn nav-action theme-toggle ${theme}`} title="switch theme" onClick={onThemeSwitch}>
 						<Icon name="sun" title="switch to dark theme" className="theme-light" size="md" />
 						<Icon name="moon" title="switch to light theme" className="theme-dark" size="md" />
 					</button>
