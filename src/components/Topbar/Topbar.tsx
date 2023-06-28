cf51285e03ae18a47affa4aa92dcbc3b00088c85
Pedro Ramos <pedrorxmosdev@gmail.com>
2023-06-28 20:13:23 +0200

    💄 feat(topbar): update nav list icons

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -23,41 +23,13 @@ export const Topbar = () => {
 						<li className="nav__item">
 							<Link to="/" className="nav__link">
 								Pokedex
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="24"
-									height="24"
-									viewBox="0 0 24 24"
-									fill="none"
-									stroke="currentColor"
-									stroke-width="2"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									className="feather feather-arrow-right"
-								>
-									<line x1="5" y1="12" x2="19" y2="12"></line>
-									<polyline points="12 5 19 12 12 19"></polyline>
-								</svg>
+								<Icon name="arrow-right" title="go to pokedex" />
 							</Link>
 						</li>
 						<li className="nav__item">
 							<Link to="/favorites" className="nav__link">
-								Favorites
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="24"
-									height="24"
-									viewBox="0 0 24 24"
-									fill="none"
-									stroke="currentColor"
-									stroke-width="2"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									className="feather feather-arrow-right"
-								>
-									<line x1="5" y1="12" x2="19" y2="12"></line>
-									<polyline points="12 5 19 12 12 19"></polyline>
-								</svg>
+								<span>Favorites</span>
+								<Icon name="arrow-right" title="go to favorites" />
 							</Link>
 						</li>
 					</ul>
