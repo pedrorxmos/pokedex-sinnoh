1cb3d79c3305824220262d1b2b2354e1ff7c8725
Pedro Ramos <pedrorxmosdev@gmail.com>
2023-06-28 23:47:19 +0200

    ✏️ fix(theme): fix localStorage hooks variables

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,13 +1,7 @@
-interface LocalStorageProps {
-	key: string;
-	value: string;
-	defaultValue: string;
-}
-
-export const useGetItem = ({ key, defaultValue }: LocalStorageProps) => {
+export const useGetItem = (key: string, defaultValue: string) => {
 	return localStorage.getItem(key) || defaultValue;
 };
 
-export const useSetItem = ({ key, value }: LocalStorageProps) => {
+export const useSetItem = (key: string, value: string) => {
 	localStorage.setItem(key, value);
 };
