f9120407070b59b455e274efe7c4860d6a9af493
Pedro Ramos <pedrorxmosdev@gmail.com>
2023-06-29 14:39:50 +0200

    💄 feat(list): pokemon card connect to pokemon api

--- src/components/PokemonCard/PokemonCard.tsx
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -1,3 +1,4 @@
+import { useFetch } from '../../hooks/useFetch';
 import './PokemonCard.scss';
 
 interface PokemonCardProps {
@@ -6,10 +7,19 @@ interface PokemonCardProps {
 }
 
 export const PokemonCard = ({ name, url }: PokemonCardProps) => {
+	// URL exmple https://pokeapi.co/api/v2/pokemon-species/389/
+	// So I separate the url by '/', and that leaves the id in the 7th spot of the array
+
+	const id = url.split('/')[6];
+	const { data } = useFetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+	const { types, stats } = data || {};
+
 	return (
-		<div>
-			{name}
-			{url}
+		<div className="pokemon-card">
+			<img
+				src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iv/diamond-pearl/${id}.png`}
+				alt={`${name} sprite`}
+			/>
 		</div>
 	);
 };
