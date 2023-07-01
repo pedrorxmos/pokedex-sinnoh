f20becd9a971a4f0f016da6e50f0faeb4e135b03
Pedro Ramos <pedrorxmosdev@gmail.com>
2023-07-01 20:02:59 +0200

    ✅ feat(test): add Pokedex page tests

--- tests/pages/Pokedex.test.tsx
+++ tests/pages/Pokedex.test.tsx
@@ -1,5 +1,39 @@
+import { render, screen } from '@testing-library/react';
+import { EntryType } from '../../src/interfaces';
+import { Pokedex } from '../../src/pages/Pokedex/Pokedex';
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+
 describe('Testing of Pokedex page', () => {
-	test('should first', () => {
-		expect(2).toBe(2);
+	const title = 'Pokedex title';
+	const entries: EntryType[] = [
+		{
+			entry_number: 1,
+			pokemon_species: {
+				name: 'turtwig',
+				url: 'https://pokeapi.co/api/v2/pokemon-species/387/',
+			},
+		},
+	];
+	const toggleFavorite = (value: EntryType) => {
+		value;
+	};
+
+	test('should show the page title', () => {
+		render(
+			<MemoryRouter>
+				<Pokedex title={title} pokedex={entries} favorites={entries} toggleFavorite={toggleFavorite} isLoading={false} />
+			</MemoryRouter>
+		);
+		expect(screen.getByText(title)).toBeTruthy();
+	});
+
+	test('should show a pokemon card', () => {
+		render(
+			<MemoryRouter>
+				<Pokedex title={title} pokedex={entries} favorites={entries} toggleFavorite={toggleFavorite} isLoading={false} />
+			</MemoryRouter>
+		);
+		expect(screen.getByText(entries[0].pokemon_species.name)).toBeTruthy();
 	});
 });
