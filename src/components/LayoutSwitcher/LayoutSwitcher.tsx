44a4a9835182c527b826a1986eda86785e435ec2
Pedro Ramos <pedrorxmosdev@gmail.com>
2023-07-01 23:54:38 +0200

    ✅ feat(test): add layoutswitcher component tests

--- src/components/LayoutSwitcher/LayoutSwitcher.tsx
+++ src/components/LayoutSwitcher/LayoutSwitcher.tsx
@@ -28,7 +28,7 @@ export const LayoutSwitcher = ({ layout, setLayout }: LayouSwitcherProps) => {
 			<div className="layout-filter">
 				<div className="layout-filter__overlay" onClick={onCloseDropdown}></div>
 				<span>View:</span>
-				<button className="layout-filter__dropdown" onClick={onToggleDropdown}>
+				<button role="layout-dropdown" className="layout-filter__dropdown" onClick={onToggleDropdown}>
 					<div className="layout-filter__dropdown__selection">
 						<Icon name={layout} title={layout} size="sm" />
 						<span>{layout}</span>
@@ -37,12 +37,12 @@ export const LayoutSwitcher = ({ layout, setLayout }: LayouSwitcherProps) => {
 					<Icon name="chevron-down" title="layout-chevron" size="sm" />
 				</button>
 				<div className="layout-filter__selection">
-					<button className="layout-filter__selection-option" onClick={() => onChangeLayout('grid')}>
+					<button role="layout-grid-btn" className="layout-filter__selection-option" onClick={() => onChangeLayout('grid')}>
 						<Icon name="grid" title="grid" size="sm" />
 						<span>Grid</span>
 					</button>
 
-					<button className="layout-filter__selection-option" onClick={() => onChangeLayout('list')}>
+					<button role="layout-list-btn" className="layout-filter__selection-option" onClick={() => onChangeLayout('list')}>
 						<Icon name="list" title="list" size="sm" />
 						<span>List</span>
 					</button>
